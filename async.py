4042ed46f4df9661b64475368b6707c83865ff10
Jean Santos <jeansaantos89@gmail.com>
2025-04-29 12:57:30 -0300

    29-04

---
+++ async.py
@@ -0,0 +1,12 @@
+import asyncio 
+from playwright.async_api import async_playwright
+
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
+        await page.goto("https://www.youtube.com/watch?v=FK_5SQPq6nY&list=PLYDwWPRvXB8_W56h2C1z5zrlnAlvqpJ6A", wait_until="load")
+        await page.screenshot(path="demo.png")
+        await browser.close()
+    
+asyncio.run(main())
