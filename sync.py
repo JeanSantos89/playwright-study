687bbf418297adc5f2170eb2eecb5142eb98483c
Jean Santos <jeansaantos89@gmail.com>
2025-04-29 13:29:08 -0300

    29-04

--- sync.py
+++ sync.py
@@ -3,15 +3,17 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p: 
     browser = p.chromium.launch(headless=False) 
     page = browser.new_page() 
-    page.goto("") # enviar o url
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login") # enviar o url
 
 # CSS SELECTOR - id #, class ., attribute tagname[attribute = "value"]
     # id: 
-    caixaDeTexto = page.wait_for_selector('#email') # espera o id email aparecer na pagina
-    caixaDeTexto.type('emailteste@gmail.com') # digita no txt box
-    
-    botaologin = page.wait_for_selector("#botao") # espera o id do botao aparecer na pagina
-    botaologin.click() # clicar no botao
+    login = page.wait_for_selector('input[name="username"]') 
+    login.type('Admin')
+    senha = page.wait_for_selector('input[type="password"]') 
+    senha.type('admin123') 
+
+    botaologin = page.wait_for_selector('button[type="submit"]') 
+    botaologin.click()
     
     page.wait_for_timeout(3000) 
     browser.close() 
